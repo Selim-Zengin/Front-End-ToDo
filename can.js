bee181069caa943d792355737ae37daa3cb00823
Selim Zengin <selimzzz321@hotmail.com>
2023-05-25 23:20:49 +0300

    Eleman ekleme tamamlandı

---
+++ can.js
@@ -0,0 +1,23 @@
+const addForm=document.querySelector('.add');
+const list=document.querySelector('.todos');
+
+const generateTmplate =todo=>{
+    const html=`
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${todo}</span>
+                <i class="fa-sharp fa-solid fa-trash delete"></i>     
+            </li>
+    `;
+    list.innerHTML+=html;
+}
+
+addForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    const todo=addForm.add.value.trim();
+    console.log(todo);
+    if(todo.length >0){
+        generateTmplate(todo);
+        addForm.reset();
+    }
+   
+})
